ef807ff853da21492d28a28f06f8afdc287bf184
vinjn <vinjn.z@gmail.com>
2021-12-18 23:19:30 +0800

    + run.bat

---
+++ ThirdPerson/run.bat
@@ -0,0 +1,1 @@
+"D:\Epic Games\UE_4.27\Engine\Binaries\Win64\UE4Editor.exe" %~dp0\ThirdPerson.uproject -tracehost=127.0.0.1 -trace=gpu,cpu,frame,log,bookmark,loadtime,file,stats,counters,memory -statnamedevents
